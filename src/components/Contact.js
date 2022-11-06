c09a3eeb44e637919ccf7697f7e3a3ea2c3663cc
Muhammed Sherif <muhammedsherif40@yahooo.com>
2022-11-06 13:00:05 +0100

    ch: made few modifications

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,11 +15,11 @@ const Contact = () => {
           <div className='flex flex-col gap-7 sm:flex-row mx-auto gap-y-6'>
             <div className=''>
               <label htmlFor="first_name" className='block mb-2 text-sm font-medium text-gray-700'>First Name</label> 
-              <input type="text" id='first_name' placeholder='Enter your first name' className='w-full sm:w-11/12 border border-[#D0D5DD] focus:outline-none py-2 px-2 rounded-lg' />
+              <input type="text" id='first_name' placeholder='Enter your first name' className='w-full sm:w-[350px] border border-[#D0D5DD] focus:outline-none py-2 px-2 rounded-lg' />
             </div>
             <div className=''>
             <label htmlFor="last_name" className='block mb-2 text-sm font-medium text-gray-700'>Last Name</label>
-            <input type="text" id='last_name' placeholder='Enter your last name' className='w-full sm:w border border-[#D0D5DD] focus:outline-none py-2 px-2 rounded-lg'/>
+            <input type="text" id='last_name' placeholder='Enter your last name' className='w-full sm:w-[350px] border border-[#D0D5DD] focus:outline-none py-2 px-2 rounded-lg'/>
             </div>
           </div>
           <div className='mt-8'>
